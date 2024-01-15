d827876f0264eeb9be46273c70bf12120689974a
xzeran2 <xzeran2@gmail.com>
2024-01-15 11:08:01 +0400

    Create Customers.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system</h2>
-    </div>
-  );
+
+const customers = [
+{ 
+  'id' : 1,
+  'image' : 'imageSample',
+  'name' : '박호철',
+  'birthday' : '961222',
+  'gender' : '남자',
+  'job' : '군무원'
+},
+{ 
+  'id' : 2,
+  'image' : 'imageSample',
+  'name' : '여동규',
+  'birthday' : '201222',
+  'gender' : '남자',
+  'job' : '장교'
+},
+{ 
+  'id' : 3,
+  'image' : 'imageSample',
+  'name' : '김선혁',
+  'birthday' : '121231',
+  'gender' : '남자',
+  'job' : '부사관'
+}
+]
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {customers.map(c=>{ return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)})}
+      </div>
+    )
+  };
 }
 
 export default App;
